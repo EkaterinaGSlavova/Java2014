f576a248a9c0db011c8ae4f950db1c3428737f87
Katia <Katia@10.1.5.36>
2014-11-23 15:06:17 +0200

    first commit

---
+++ 1401417005_EkaterinaSlavova/Vtorouprajnenie1/src/vtorouprajnenie1/Vtorouprajnenie1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vtorouprajnenie1;
+
+/**
+ *
+ * @author Katia
+ */
+public class Vtorouprajnenie1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /*int i = 1;
+        while (i<11) {
+            System.out.println("i="  +i);
+            i++;
+        }
+        */
+        /*for(int i=20; i>0; i=i-1)
+
+        System.out.println(i);
+        */
+         int i;
+
+        for(i=1; i<=5; i++)
+
+            System.out.println('a');
+
+        for(i=1; i<=10; i++)
+
+            System.out.println('b');
+    }
+    
+}
